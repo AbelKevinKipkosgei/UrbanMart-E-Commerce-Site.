0d45974fa161a20e1d51b3f0884ca86bce0d7d67
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-10-27 10:08:57 +0300

    Removed unused imports.

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-from sqlite3 import IntegrityError
-import traceback
 from flask import jsonify, make_response, render_template, request
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 from flask_restful import Resource
