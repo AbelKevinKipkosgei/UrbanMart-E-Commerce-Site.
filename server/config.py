ab7a9544bf08bb91e743fb2ffbf1aff3bdd4e77a
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-10-27 08:35:35 +0300

    Modified to use PostgreSQL from Render.

--- server/config.py
+++ server/config.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
@@ -5,8 +7,16 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urban_mart.db'
+load_dotenv()
+
+app = Flask(
+    __name__,
+    static_url_path='',
+    static_folder='../client/build',
+    template_folder='../client/build'
+)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urban_mart.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
